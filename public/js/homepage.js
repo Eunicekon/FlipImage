9347744f245ad1be4aaa04a20c953b3c69a23a0c
Eunice Kontor <Eunice.Kontor@ft.com>
2021-01-02 12:25:11 +0000

    deploying to heroku

---
+++ public/js/homepage.js
@@ -0,0 +1,7 @@
+// let signUp = document.getElementById('sign-up').onclick = function (){
+//   location.href = './signup'
+// }
+
+// let signIp = document.getElementById('sign-in').onclick = function (){
+//   location.href = './signin'
+// }
