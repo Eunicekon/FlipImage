a98cc4d7a0134df24be74eb5b2347ac25f1d698b
Eunice Kontor <Eunice.Kontor@ft.com>
2021-01-02 13:19:31 +0000

    test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb://localhost/FlipImage', {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('we are connected!');
+  console.log('Database is connected :) !');
 });
 
 //EJS 
