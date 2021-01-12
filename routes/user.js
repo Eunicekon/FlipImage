e47e16cde15dc4738fb02ada8930b240bcc81111
Eunice Kontor <Eunice.Kontor@ft.com>
2021-01-12 18:08:15 +0000

    collected data from signup page shown correctly on terminal

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
-const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
-const User = require("../models/users.js");
+const User = require("../models/user.js");
 
 // GET REQUEST: sign in handle
 router.get('/signin', (req, res) => {
@@ -19,10 +18,10 @@ router.post('/signup', (req, res) => {
   if(!name || !username || !email || !password){
     errors.push({msg: "Please fill in all the available fields"})
   }
-  if(password.lenght < 6){
+  if(password.length < 6){
     errors.push({msg: 'password is not long enough! Max is 6 characters!'})
   }
-  if(errors.lenght > 0){
+  if(errors.length > 0){
     res.render('signup', {
       errors: errors,
       name: name,
@@ -41,8 +40,21 @@ router.post('/signup', (req, res) => {
           name: name,
           username: username,
           email: email,
-          passowrd: password
+          password: password,
         });
+        //line 46-50 : Generate a salt, hash the user’s password. Assign this encrypted value to the user’s password and then save the client to the database.
+          bcrypt.genSalt(10,(err,salt) =>
+          bcrypt.hash(newUser.password,salt,
+            (err,hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save()
+            .then((value) => {
+              console.log(value)
+              res.redirect('/user/login');
+            })
+            .catch(value => console.log(value));
+        }));
       }
     })
   }
@@ -50,7 +62,6 @@ router.post('/signup', (req, res) => {
 router.post('/signin', (req, res, next) => {
 })
 
-//log out handle
 router.get('/logout', (req, res) => {
 })
 
